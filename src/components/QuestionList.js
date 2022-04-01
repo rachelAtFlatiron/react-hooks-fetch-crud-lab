4a6a7218826f1b4e988b1e282b732c20dba8b119
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-01 14:20:26 -0400

    get questions and display in view questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from './QuestionItem';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async() => {
+      await fetch("http://localhost:4000/questions")
+      .then(res => res.json())
+      .then(res => setQuestions(res))
+    }
+    fetchData();
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {
+          questions.map((el, i) => {
+            return <QuestionItem key={i} question={el} />
+          })
+        }
+      </ul>
     </section>
   );
 }
